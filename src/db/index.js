0f0b6b7eaedf6441d2df683e928da6db1fa4fa72
Darsh0100 <darshtiwari71@gmail.com>
2025-08-20 00:02:49 +0530

    new changes

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose ,{connect} from  "mongoose"
+import { DB_NAME } from "../constants.js"
+const connectdb = async()=>{
+
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`);
+       console.log("hogya connection!!");
+    }
+    catch(error){
+      console.log("MONGO DB CONNECTION FAILED AWW:",error);
+      process.exit(1);
+
+    }
+}
+
+export default connectdb;
+
