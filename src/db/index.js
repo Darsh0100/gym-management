136fa4612954eb8e22b7faa6d998afe602f2c2bd
me-mayank <174928744+me-mayank@users.noreply.github.com>
2025-08-22 03:27:56 +0530

    solved the connection issure in db/index file

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose, { connect } from "mongoose";
 import { DB_NAME } from "../constants.js";
 const connectdb = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
     console.log("MONGODB CONNECTED!!");
   } catch (error) {
     console.log("MONGO DB CONNECTION FAILED AWW:", error);
